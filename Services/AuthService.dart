38467bec6dc969c0e26ee005309382d578226499
Mohamed amine Tlili <foxweb585@gmail.com.com>
2023-11-03 22:46:41 +0100

    1st

--- Services/AuthService.dart
+++ Services/AuthService.dart
@@ -1,1 +1,8 @@
-class Authservice {}
+class Authservice {
+  /*Future<Student>*/ FirebaseSignin() async {}
+  /*Future<Student>*/ FirebaseSignup() async {}
+  /*Future<Student>*/ GoogleSignin() async {}
+  /*Future<Student>*/ GoogleSignup() async {}
+  /*Future<Student>*/ Signout() async {}
+  /*Future<Student>*/
+}
